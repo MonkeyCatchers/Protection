21a7527628184df473866a6ffe59b30e689a908c
MonkeyCatchers <110670058+MonkeyCatchers@users.noreply.github.com>
2022-08-08 13:47:03 +1000

    Update src.lua

--- Executors/src.lua
+++ Executors/src.lua
@@ -1,6 +1,19 @@
 local NOTSUPPORTEDEXECUTORS = {
     ['WRD-API'] = "JJSploit"
     ['Arceus'] = "Arceus X"
+    ['Fluxus'] = "Fluxus"
+    ['ScriptWare'] = "Script Ware"
+    [''] = "Trigon"
+    [''] = "Temple"
+    [''] = "Electron"
 }
 
-return NOTSUPPORTEDEXECUTORS
+local SUPPORTEDEXECUTORS = {
+    ['Synapse X'] = "Synapse X"
+    ['Fluxus'] = "Fluxus"
+    ['ScriptWare'] = "Script Ware"
+}
+
+local WEIRDSUPPORTEDEXECUTORS = {
+    ['Krnl'] = "KRNL"
+}
